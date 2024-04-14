908042d8bad9122a7011d2e04810265ec720d4e9
Ayush Agarwal <41159085+ayush-96@users.noreply.github.com>
2024-04-15 01:13:34 +0530

    Create Most_Profitable_Companies.sql
    forbes_global_2010_2014
    0.00682 seconds

---
+++ sql_medium/Most_Profitable_Companies.sql
@@ -0,0 +1,11 @@
+-- Find the 3 most profitable companies in the entire world.
+-- Output the result along with the corresponding company name.
+-- Sort the result based on profits in descending order.
+
+-- select * from forbes_global_2010_2014;
+
+SELECT company,
+    profits
+FROM forbes_global_2010_2014
+ORDER BY profits DESC
+LIMIT 3;
