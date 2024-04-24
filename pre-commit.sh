74c4bd58c44307a7f563d41a7971e647de5e26fb
Ayush Agarwal <agarwalayush2396@gmail.com>
2024-04-25 00:06:06 +0530

    test 5

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-chmod +x pre-commit.sh
+echo "test sample"
+#chmod +x pre-commit.sh
 
 # Get the name of the current branch
-current_branch=$(git rev-parse --abbrev-ref HEAD)
+#current_branch=$(git rev-parse --abbrev-ref HEAD)
 
 # Name of the output file
-output_file="branch_diff.txt"
+#output_file="branch_diff.txt"
 
 # Perform the diff with the main branch and write output to a file
-git diff main "$current_branch" > "$output_file"
+#git diff main "$current_branch" > "$output_file"
 
 # Inform the user about the output file
-echo "Git diff between main branch and $current_branch saved to: $output_file"
+#echo "Git diff between main branch and $current_branch saved to: $output_file"
